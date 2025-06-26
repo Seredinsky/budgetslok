ef4b19e70f89c28c3d5843e24853dcd42d76bf22
Andrey <seredinsky@hotmail.com>
2025-06-27 01:55:00 +0300

    v.9.18

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -34,8 +34,7 @@ urlpatterns = [
     re_path(r"^(?:.*)/?$", TemplateView.as_view(template_name="index.html"), name="spa"),
 ]
 
-# Раздаём загруженные файлы в режиме DEBUG
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+# Serve media files at MEDIA_URL
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
