9705f5773daec82dc92e3df506ab1faa862e1c7e
Andrey <seredinsky@hotmail.com>
2025-06-26 15:36:38 +0300

    v.8.1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,9 +142,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # здесь будет React‑build
+_react_static = BASE_DIR / "static"
+STATICFILES_DIRS = [_react_static] if _react_static.exists() else []
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
